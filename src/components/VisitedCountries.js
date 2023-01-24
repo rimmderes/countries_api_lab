b23d9ab922054cf8746bf14579cfb7cb9d13322c
Loshanth Selvanayagam <loshanth75@googlemail.com>
2023-01-24 19:52:08 +0000

    added checkboxes

--- src/components/VisitedCountries.js
+++ src/components/VisitedCountries.js
@@ -1,3 +1,19 @@
+import CountriesList from "./CountriesList";
+import Country from "./Country";
 const VisitedCountries = () => {
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    const addVisitedCountries = (country) => {
+
+        setVisitedCountries = ([...visitedCountries, country])
+    }
+
+    return (
     
-}
+            visitedCountries ? visitedCountries.map(function(country) 
+            
+            )
+        )
+
+    
+} 
