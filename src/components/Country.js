f31086efc14c1ccfc32500770bc78d7c11a528e0
Radhika <radhikarathi308@gmail.com>
2023-01-24 19:26:49 +0000

    adds checkbox

--- src/components/Country.js
+++ src/components/Country.js
@@ -2,21 +2,26 @@ import CountriesList from "./CountriesList";
 
 const Country = ({country}) => {
 
+const handleChange = () => {
+     
+}
+     
      return (
           <div>
           <ul>
                <li>
-                    Name: {country.name.common} {country.flag}
+                    <input type="checkbox" name="Visited?" onChange={handleChange}/>
+                    {country.name.common} {country.flag}
                </li>
                
                     Population: {country.population}
                  
             </ul>
             </div>
+            
      )
 
 }
-
 
 
 export default Country;
